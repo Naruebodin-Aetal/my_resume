2c74efaf8c40f1308caf65a6e21da4d165cc7030
Naruebodin-Aetal <naruebodin.a@ku.th>
2025-08-02 22:57:02 +0700

    final_edit

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,13 @@ class MyApp extends StatelessWidget {
                 left: Radius.circular(20),
                 right: Radius.circular(20),
               ),
-              //isScrollable: true,
+              isScrollable: true,
               tabs: [
-                Tab(icon: Icon(Icons.directions_car), text: "Car"),
-                Tab(icon: Icon(Icons.directions_transit), text: "Train"),
-                Tab(icon: Icon(Icons.directions_bike), text: "bike"),
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit))
+                Tab(icon: Icon(Icons.account_box), text: "Profile"),
+                Tab(icon: Icon(Icons.contact_mail), text: "Contact"),
+                Tab(icon: Icon(Icons.build), text: "Skills"),
+                Tab(icon: Icon(Icons.school), text: "Education"),
+                Tab(icon: Icon(Icons.videogame_asset_rounded), text: "Hobbies"),
               ],
             ),
             title: const Text('My Resume'),
@@ -62,14 +62,14 @@ class MyApp extends StatelessWidget {
       child: Container(
         // กรอบทั้งหมด
         margin: EdgeInsets.all(40),
-        padding: EdgeInsets.all(20),
+        padding: EdgeInsets.all(30),
         decoration: BoxDecoration(
           border: Border.all(color: Colors.brown, width: 2),
           borderRadius: BorderRadius.circular(15),
           color: Colors.white,
         ),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisSize: MainAxisSize.max,
           children: [
             Container(
               decoration: BoxDecoration(
@@ -219,6 +219,31 @@ class MyApp extends StatelessWidget {
                 ),
               ],
             ),
+
+            SizedBox(height: 10), // ระยะห่างระหว่างหัวข้อกับเนื้อหา
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Email: ',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'pao0992174774@gmail.com',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w200,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
@@ -236,7 +261,116 @@ class MyApp extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
           color: Colors.white,
         ),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Text(
+              'ความสามารถในด้านต่าง ๆ',
+              style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              ),
+            ),
+            
+            SizedBox(height: 10), // ระยะห่างระหว่างหัวข้อกับเนื้อหา
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              
+              Text(
+                'ภาษาที่ใช้สื่อสาร: ',
+                style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                ),
+              ),
+              Text(
+                'ไทย– ระดับเจ้าของภาษา\nอังกฤษ– อ่าน/เขียน/สนทนา ระดับดี',
+                style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w200,
+                color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 10), // ระยะห่างระหว่างหัวข้อกับเนื้อหา
+              Text(
+                'ภาษาคอมพิวเตอร์: ',
+                style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                ),
+              ),
+              ],
+            ),
+            GridView.count(
+              crossAxisCount: 2,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              childAspectRatio: 6, // ทำให้ช่องสูงน้อยลง เลขมากยิ่งแคบ
+              mainAxisSpacing: 6, // ระยะห่างแนวตั้ง
+              crossAxisSpacing: 10, // ระยะห่างแนวนอน
+              children: [
+                Text('•C++', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black)),
+                Text('•Java', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black)),
+                Text('•Python', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black)),
+                Text('•JavaScript', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black)),
+                Text('•HTML', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black)),
+                //Text('•CSS', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black)),
+              ],
+            ),
+            SizedBox(height: 10), // ระยะห่างระหว่างหัวข้อกับเนื้อหา
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              
+              Text(
+                'ฐานข้อมูล: ',
+                style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                ),
+              ),
+              Text(
+                '-MySQL',
+                style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w200,
+                color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 10), // ระยะห่างระหว่างหัวข้อกับเนื้อหา
+              Text(
+                'เครื่องมือที่ใช้: ',
+                style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                ),
+              )
+              ],
+            ),
+            GridView.count(
+              crossAxisCount: 2,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              childAspectRatio: 6, // ทำให้ช่องสูงน้อยลง เลขมากยิ่งแคบ
+              mainAxisSpacing: 6, // ระยะห่างแนวตั้ง
+              crossAxisSpacing: 10, // ระยะห่างแนวนอน
+              children: [
+                Text('•GitHub', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black)),
+                Text('•Docker', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black)),
+                Text('•Postman', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black)),
+                Text('•Visual Studio Code', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black)),
+              ],
+            ),
+          ],
+        ),
       ),
+      
     );
   }
 
@@ -250,6 +384,70 @@ class MyApp extends StatelessWidget {
           border: Border.all(color: Colors.brown, width: 2),
           borderRadius: BorderRadius.circular(15),
           color: Colors.white,
+        ),
+        child: Column(
+          children: [
+            Text(
+              'ระดับการศึกษา',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: SingleChildScrollView(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: certificates.length,
+                  itemBuilder: (context, index) {
+                    final certificate = certificates[index];
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 200,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                flex: 3,
+                                child: Container(
+                                  width: double.infinity,
+                                  child: Image.asset(
+                                    certificate.imagePath,
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Expanded(
+                                flex: 2,
+                                child: Container(
+                                  width: double.infinity,
+                                  child: Text(
+                                    certificate.description,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w200,
+                                      color: Colors.black,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -291,12 +489,16 @@ class MyApp extends StatelessWidget {
                 ),
                 Container(
                   padding: const EdgeInsets.all(8),
-                  child: Center(child: const Text('อ่านหนังสือ/\nอ่านการ์ตูน')),
+                  child: Center(child: const Text('อ่านหนังสือ/\nอ่านการ์ตูน',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black),
+                  )),
                 ),
 
                 Container(
                   padding: const EdgeInsets.all(8),
-                  child: Center(child: const Text('เล่นเกม')),
+                  child: Center(child: const Text('เล่นเกม',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200, color: Colors.black),
+                  )),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -315,3 +517,32 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class certificate {
+  final String description;
+  final String imagePath;
+
+  certificate({
+    required this.description,
+    required this.imagePath,
+  });
+}
+
+final List<certificate> certificates = [
+  certificate(
+    description: 'ปัจจุบันกำลังศึกษาในระดับปริญญาตรี\nอยู่ที่มหาวิทยาลัยเกษตรศาสตร์\nวิทยาเขตกำแพงแสน',
+    imagePath: 'images/ku_logo.png',
+  ),
+  certificate(
+    description: 'เข้าอบรมหลักสูตร เปิดโลกทักษะAI และความปลอดภัยทางไซเบอร์',
+    imagePath: 'images/139_page-0001.jpg',
+  ),
+  certificate(
+    description: 'ประกาศนียบัตรการใช้ CSS',
+    imagePath: 'images/css_certificate_page-0001.jpg',
+  ),
+  certificate(
+    description: 'ประกาศนียบัตรการใช้ SQL',
+    imagePath: 'images/sql_basic_certificate_page-0001.jpg',
+  ),
+];
